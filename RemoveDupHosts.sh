5992e4f55bc7b49f3567ae09eb5975bea8898939
Chris House <76433408+chouseitrs@users.noreply.github.com>
2021-10-08 13:21:21 -0600

    Update RemoveDupHosts.sh
    updated bug fix

--- RemoveDupHosts.sh
+++ RemoveDupHosts.sh
@@ -29,33 +29,30 @@ totalhost=$(curl -k -s --request GET \
   --header "Authorization: Basic $authstr" )
 
 let totalhost=$(echo $totalhost | grep 'count' | cut -d: -f2 | sed 's/}//g')
-echo $totalhost
 
 ##Run with offset if $totalhost > $limit
-if [[ $totalhost -gt $limit ]]; then
-    while [[ $offset -le $totalhost ]]; do
-        echo $offset" : "$limit" : "$totalhost
-        ##Get all duplicate hosts into a single file
-        if [[ $offset == 0 ]]; then
-            limoff="limit=$limit"
-        else
-            limoff="limit=$limit&offset=$offset"
-        fi
-        ##API call
-        allhosts=$(curl -k -s --request GET \
-        --url "https://$master:443/api/filter/query?columns=name%2Caddress&$limoff&query=%5Bhosts%5D%20all" \
-        --header "Authorization: Basic $authstr" \
-        --header 'accept: application/json')
-        ##Loop and Filter for duplicates. Only write host with IP as name to file.
-        for i in `echo $allhosts | sed 's/\]//g' | sed 's/\[//g' |  sed 's/}\,{/\n/g'`; \
-            do 
-            i=$(echo $i | sed 's/{//g' | sed 's/}//g')
-            if [[ $(echo $allhosts | grep -o $(echo $i | awk -F'[,:]' '{print $4}') 2>/dev/null | wc -l) > 1 && \
-            $(echo $i | awk -F'[,:]' '{print $2}') == $(echo $i | awk -F'[,:]' '{print $4}') ]]; \
-            then echo $i | sed 's/{//g' >> /tmp/duplicateHosts.txt; fi ; done
-        let offset=$offset+$limit
-    done
-fi
+while [[ $offset -le $totalhost ]]; do
+    ##Get all duplicate hosts into a single file
+    if [[ $offset == 0 ]]; then
+        limoff="limit=$limit"
+    else
+        limoff="limit=$limit&offset=$offset"
+    fi
+    ##API call
+    allhosts=$(curl -k -s --request GET \
+    --url "https://$master:443/api/filter/query?columns=name%2Caddress&$limoff&query=%5Bhosts%5D%20all" \
+    --header "Authorization: Basic $authstr" \
+    --header 'accept: application/json')
+    ##Loop and Filter for duplicates. Only write host with IP as name to file.
+    for i in `echo $allhosts | sed 's/\]//g' | sed 's/\[//g' |  sed 's/}\,{/\n/g'`; \
+        do 
+        i=$(echo $i | sed 's/{//g' | sed 's/}//g')
+        if [[ $(echo $allhosts | grep -o $(echo $i | awk -F'[,:]' '{print $4}') 2>/dev/null | wc -l) > 1 && \
+        $(echo $i | awk -F'[,:]' '{print $2}') == $(echo $i | awk -F'[,:]' '{print $4}') ]]; \
+        then echo $i | sed 's/{//g' >> /tmp/duplicateHosts.txt; fi ; done
+    let offset=$offset+$limit
+done
+
 
 ##Loop through duplicate hosts and submit API call to remove the host with ip address as name
 while read j;
